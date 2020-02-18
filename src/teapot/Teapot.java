a5f869358fa565ed9bd2cf17d683c0675bab284c
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-02-18 02:10:42 +0100

    teapot can store data now

--- src/teapot/Teapot.java
+++ src/teapot/Teapot.java
@@ -11,25 +11,34 @@ import javacard.framework.*;
  * Filename: Teapot.java 
  * Class: Teapot
  */
-public class Teapot extends Applet
-{
+public class Teapot extends Applet{
+
    // Define the value of CLA/INS in APDU, you can also define P1, P2.
-   private static final byte CLA_DEMO_TEST            = (byte)0xB0;
-   private static final byte INS_DEMO_TMP1            = (byte)0x11;
-   private static final byte INS_DEMO_TMP2            = (byte)0x12;
-    
+   private static final byte CLA_TEAPOT               = (byte)0xB0;
+   private static final byte INS_GET                  = (byte)0xA1;
+   private static final byte INS_PUT                  = (byte)0xA2;
+
+   private static final byte MAX_SIZE                 = (byte)32;
+
+   // Default secret
+   private byte[] secret = {
+      'I',' ','a','m',' ','a',' ','t',
+      'e','a','p','o','t',' ','g','i',
+      'm','m','e',' ','s','o','m','e',
+      ' ','t','e','a',' ','p','l','z'
+   };
+   private byte secretlen = (byte)32;
+
    // Create an instance of the Applet subclass using its constructor, 
    // and to register the instance.
-   public static void install(byte[] bArray, short bOffset, byte bLength) 
-   {
+   public static void install(byte[] bArray, short bOffset, byte bLength){
       // Only one applet instance can be successfully registered each time 
       // the JCRE calls the Applet.install method.
       new Teapot().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
    }
    // Process the command APDU, 
    // All APDUs are received by the JCRE and preprocessed. 
-   public void process(APDU apdu)
-   {
+   public void process(APDU apdu){
       // Select the Applet, through the select method, this applet is selectable, 
       // After successful selection, all APDUs are delivered to the currently selected applet
       // via the process method.
@@ -42,27 +51,28 @@ public class Teapot extends Applet
       // Calling this method indicates that this APDU has incoming data. 
       apdu.setIncomingAndReceive();
       
-      // If the CLA is not equal to 0xB0(CLA_DEMO_TEST),  throw an exception.
-      if(buf[ISO7816.OFFSET_CLA] != CLA_DEMO_TEST)
-      {
+      // If the CLA is not equal to 0xB0(CLA_TEAPOT),  throw an exception.
+      if(buf[ISO7816.OFFSET_CLA] != CLA_TEAPOT){
          ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
       }
       // Dispatch INS in APDU.
-      switch (buf[ISO7816.OFFSET_INS])
-      {
-         // The APDU format can be "B0 11 P1 P2 Lc Data Le", 
-         // such as "B0110000" or "B01101020311223300".
-      case INS_DEMO_TMP1:
+      switch (buf[ISO7816.OFFSET_INS]){
+         // The APDU format can be "B0 A1 P1 P2 Lc Data Le", 
+         // such as "B0A10000" or "B0A101020311223300".
+      case INS_GET:
          SendData(apdu);
          break;
-         // The APDU format can be "B0 12 P1 P2 Lc Data Le",
-         // such as "B0125566" or "B012000002112200".
-      case INS_DEMO_TMP2:
-         // Set 0x5555 into the 'buf' array, the offset is 0.
-         Util.setShort(buf, (short)0, (short)0x5555);
-         // Send the first 2 bytes data in 'buf',
-         // the hex of JCRE sending data is "55 55 90 00".
-         apdu.setOutgoingAndSend((short)0, (short)2);
+         // The APDU format can be "B0 A2 P1 P2 Lc Data Le",
+         // such as "B0A2000002112200".
+         // Up to 32 bytes
+      case INS_PUT:
+         if(buf[ISO7816.OFFSET_LC] > MAX_SIZE){
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+         }
+         // copy content of the buffer to the secret array
+         secretlen = buf[ISO7816.OFFSET_LC];
+         Util.arrayCopy(buf, (short)ISO7816.OFFSET_CDATA, secret, (short)0, (short)secretlen);
+         SendData(apdu);
          break;
       default:
          // If you don't know the INS, throw an exception.
@@ -75,24 +85,16 @@ public class Teapot extends Applet
    {
       // Get the apdu buffer datas again.
       byte [] buffer = apdu.getBuffer();
-      
-      // Define a byte string with the message "Hello World"
-      byte sendStr[] = {
-         'I',' ','a','m',' ','a',' ','t',
-         'e','a','p','o','t',' ','g','i',
-         'm','m','e',' ','s','o','m','e',
-         ' ','t','e','a',' ','p','l','z'
-      };
-      short len = (short) sendStr.length;
-      // Copy "Hello world" character to APDU Buffer.
-      Util.arrayCopyNonAtomic(sendStr, (short)0, buffer, (short)0, (short)len);
+
+      // Copy secret to APDU Buffer.
+      Util.arrayCopyNonAtomic(secret, (short)0, buffer, (short)0, secretlen);
          
       // Set the data transfer direction to outbound.
       apdu.setOutgoing();
       // Set the actual length of response data.
-      apdu.setOutgoingLength(len);
+      apdu.setOutgoingLength(secretlen);
       // Sends the data of APDU buffer 'buffer', the length is 'len' bytes,  the offset is 0.
-      apdu.sendBytes((short) 0, (short)len);
+      apdu.sendBytes((short)0, secretlen);
    }
 
 }
