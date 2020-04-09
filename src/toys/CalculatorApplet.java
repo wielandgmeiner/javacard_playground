f04a7af71cee96cb7b1d8a3172a3b309eb90dab1
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-04-09 05:15:42 +0200

    constant time modulo addition

--- src/toys/CalculatorApplet.java
+++ src/toys/CalculatorApplet.java
@@ -18,6 +18,11 @@ public class CalculatorApplet extends Applet{
     private static final byte INS_HMAC_SHA256          = (byte)0xA2;
     private static final byte INS_SHA512               = (byte)0xA3;
     private static final byte INS_HMAC_SHA512          = (byte)0xA4;
+    // finite field math
+    private static final byte INS_ADDMOD_FP            = (byte)0xA5;
+    private static final byte INS_ADDMOD_N             = (byte)0xA6;
+
+    private byte[] scratch;
 
     // Create an instance of the Applet subclass using its constructor, 
     // and to register the instance.
@@ -30,6 +35,7 @@ public class CalculatorApplet extends Applet{
     public CalculatorApplet(){
         Secp256k1.init();
         Crypto.init();
+        scratch = new byte[32];
     }
     // Process the command APDU, 
     // All APDUs are received by the JCRE and preprocessed. 
@@ -92,12 +98,71 @@ public class CalculatorApplet extends Applet{
             Crypto.hmacSha512.doFinal(buf, (short)(offset+1), buf[offset], buf, (short)0);
             apdu.setOutgoingAndSend((short)0, (short)64);
             break;
+        case INS_ADDMOD_FP:
+            // TODO: check that arguments are 32 byte long
+            if(numElements != 2){
+                ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+            }
+            addMod(buf, (short)(offset+1), buf, (short)(offset+34), buf, (short)0, Secp256k1.SECP256K1_FP, (short)0);
+            apdu.setOutgoingAndSend((short)0, (short)32);
+            break;
+        case INS_ADDMOD_N:
+            // TODO: check that arguments are 32 byte long
+            if(numElements != 2){
+                ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+            }
+            addMod(buf, (short)(offset+1), buf, (short)(offset+34), buf, (short)0, Secp256k1.SECP256K1_R, (short)0);
+            apdu.setOutgoingAndSend((short)0, (short)32);
+            break;
         default:
             // If you don't know the INS, throw an exception.
             buf[0] = numElements;
             apdu.setOutgoingAndSend((short)0, (short)1);
             // ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
         }
+    }
+    // constant time modulo addition
+    private void addMod(byte[] a,     short aOff, 
+                        byte[] b,     short bOff, 
+                        byte[] out,   short outOff, 
+                        byte[] prime, short pOff){
+        // actual addition
+        short carry = 0;
+        for(short i=31; i>=0; i--){
+            carry = (short)((short)(a[(short)(aOff+i)]&0xFF)+(short)(b[(short)(bOff+i)]&0xFF)+carry);
+            scratch[i] = (byte)carry;
+            carry = (short)(carry>>8);
+        }
+        // modulo subtraction:
+        // subtract in any case and store result in output buffer
+        short scarry = sub(scratch, (short)0, prime, pOff, out, outOff);
+        // check if we needed to subtract
+        if(carry!=0 || scarry==0){
+            // we are fine, but we need to copy something, 
+            // so let's copy output buffer to temp buffer
+            Util.arrayCopyNonAtomic(out, outOff, scratch, (short)0, (short)32);
+        }else{
+            // there was no overflow - copy from temp buffer to output
+            Util.arrayCopyNonAtomic(scratch, (short)0, out, outOff, (short)32);
+        }
+    }
+    // constant time modulo subtraction
+    private short sub(byte[] a, short aOff, 
+                     byte[] b, short bOff,
+                     byte[] out, short outOff){
+        short carry = 0;
+        for(short i=31; i>=0; i--){
+            carry = (short)((a[(short)(aOff+i)]&0xFF)-(b[(short)(bOff+i)]&0xFF)-carry);
+            out[(short)(outOff+i)] = (byte)carry;
+            carry = (short)(((carry>>8)!=0) ? 1 : 0);
+        }
+        return carry;
+    }
+    // constant time comparison
+    private boolean isGreater(byte[] a, short aOff,
+                              byte[] b, short bOff){
+        // if a more than b, b-a will be negative - we will get non-zero carry
+        return (sub(b, bOff, a, aOff, scratch, (short)0)!=0);
     }
     // checks that buffer contains a list of elements
     // encoded as <len><data><len><data>...
