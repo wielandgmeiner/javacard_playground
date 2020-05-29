39478d40535f14495112af04a0253cab84080ec3
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-05-29 11:47:06 +0200

    comments for TeapotApplet and DataEntry

--- src/main/java/toys/DataEntry.java
+++ src/main/java/toys/DataEntry.java
@@ -1,18 +1,35 @@
-/* Do what the fuck you want license. */
 package toys;
 
 import javacard.framework.Util;
 
-/* 
- * Package: toys
- * Filename: DataEntry.java 
- * Class: DataEntry
+/**
+ * A class to store variable length data in EEPROM.
+ * In the constructor define the maximum capacity 
+ * that will be allocated for storage.
+ * <p>
+ * Constructor is called with a single parameter - maximum capacity.
+ * <pre>
+ * // Usage example
+ * DataEntry de = new DataEntry(maxLen); // create DataEntry with max capacity of maxLen
+ * de.put(data, offset, len); // store data in the storage
+ * byte[] buf = de.get();    // access bytearray of the storage,
+ * short len = de.length();  // figure out the length of data stored,
+ * short maxLen = de.maxLength(); // find maximum capacity of the storage
+ * </pre>
  */
 public class DataEntry{
+    /** Byte buffer to store data in. Allocated in the constructor */
     private byte[] buffer;
+    /** Length of the data currently stored */
     private short bufferLength = (short)0;
+    /** Maximum length of the data we can store. Defined in the constructor. */
     private short bufferMaxLength = (short)0;
 
+    /**
+     * Class constructor. Allocates enough memory in EEPROM to store data.
+     * @param maxSize - size that will be allocated for storage. 
+     *                  Defines the maximum length of the data that we can store.
+     */
     public DataEntry(short maxSize){
         // if maxSize < 0 -> throw error
         bufferMaxLength = maxSize;
@@ -42,19 +59,20 @@ public class DataEntry{
         return buffer;
     }
     /**
-     * @return length of the data
+     * @return length of the data currently stored
      */
     public short length(){
         return bufferLength;
     }
     /**
-     * @return buffer capacity
+     * @return maximum length of the data that can be stored in this class 
+     *         (internal buffer capacity)
      */
     public short maxLength(){
         return bufferMaxLength;
     }
     /**
-     * Erases content of the data with random junk
+     * Overwrites the content of the internal buffer with zeroes
      */
     public void wipe(){
         Util.arrayFillNonAtomic(buffer, (short)0, bufferMaxLength, (byte)0x00);
